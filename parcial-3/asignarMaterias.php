b2e2d6dbd2d9413f74f08c5b17aab45b72affd2a
JoseLuisZN3 <124735572+JoseLuisZN3@users.noreply.github.com>
2023-11-14 19:21:58 -0700

    Add files via upload

--- parcial-3/asignarMaterias.php
+++ parcial-3/asignarMaterias.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>asigmar Materias</title>
+    <title>Asignar materias</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="code.jquery.com_jquery-3.7.1.js"></script>
 </head>
@@ -12,47 +11,47 @@
     <?php
         include 'menu.php';
         include 'conexion.php';
-            $sql_alumnos = "SELECT * FROM alumnos";
-            $sql_materias = "SELECT * FROM  materias";
-            $datos_alumnos = $conexion->query($sql_alumnos);
-            $datos_materias = $conexion->query($sql_materias);
+        $sql_alumnos ="SELECT * FROM alumnos";
+        $sql_materias = "SELECT *FROM materias";
+        $datos_alumnos = $conexion->query($sql_alumnos);
+        $datos_materias = $conexion->query($sql_materias);
     ?>
+
     <div class="container">
         <div class="row">
-            <div class="col-12 card p-4m-2">
-                <h2>Asignar materias a alumnos</h2><br>
-                <form action="">
-                    <label for="">Alumno:</label>
+            <div class="col-12 card p-4 m-2">
+                <h1>Asignar materias a alumno</h1>
+                <form action="GuardarAsignacion.php" method="POST">
                     <select name="alumno" class="form-control">
-                        <option value="">selecciona un alumno</option>
-                        <?php
-                            if($datos_alumnos->num_rows > 0){
-                            while($registro = $datos_alumnos->fetch_assoc()){ ?>
-                            <option value="<?php echo $registro["id"];?>"><?php echo $registro["nombre"]; ?></option>
-
-                            <?php } ?>
+                    <?php if( $datos_alumnos->num_rows>0){
+                        while($registro = $datos_alumnos->fetch_assoc()){ ?>
+                            <option value="<?php echo $registro["id"]; ?>"><?php echo $registro["nombre"]; ?></option> 
                         <?php } ?>
-                    </select>                    
-                <h3>Seleccione las materias:</h3><hr>
-                <?php if($datos_materias->num_rows > 0) { 
-                    while($registro = $datos_materias->fetch_assoc()){ ?>
-                    <p><input type="checkbox" name="materias[]" value=""> <?php echo $registro["nombre"]; ?></p>
+                    <?php } ?>
 
-                <?php } } ?>
-                        <div>
-                            <input type="submit" class="btn btm-primary" value="asignar">
+                    </select>
+                    <h3>seleccione las materias</h3>
+                    <?php if( $datos_alumnos->num_rows>0){
+                         while($registro = $datos_materias->fetch_assoc()){ ?>
+                         <p><input type="checkbox" name="materias[]" value="<?php echo $registro["id"]; ?>"> <?php echo $registro["nombre"];?></p>
+                         <?php } } ?>
+                         
+                         <div>
+                            <input type="submit" class="btn btn-primary" value="Asignar">
                             <a href="" class="btn btn-danger">Cancelar</a>
-                        </div>
-
+                         </div>
+                        
                 </form>
             </div>
         </div>
     </div>
+
     <footer class="text-center">
         <hr>
-        CETIS107 copy;2023
-
+        Cetis 107 &copy; 2023
     </footer>
-    
+
+    <script src="js/bootstrap.bundle.js"></script>  
+
 </body>
 </html>
